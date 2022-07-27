86a7064536912fd2a3572ed8de95a54f23b8e059
Allyson Nasworthy <allyson.nasworthy@gmail.com>
2022-07-27 10:29:18 -0400

    fix 5day call, hover css added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,82 +2,14 @@ var apiKey = "87aae122cd0cc6a9c404e4fd528a2e4d";
 var today = moment().format('LL');
 var searchHistoryList = [];
 
-// function for current condition
-var currentCondition = function(city) {
-
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-
-    fetch(apiURL)
-    .then(function(response) {
-        if (response.ok) {
-        console.log(response);
-        response.json().then(function(cityWeatherResponse) {
-        console.log(cityWeatherResponse)
-        $("#weatherContent").css("display", "block");
-        $("#cityDetail").empty();
-        
-        var iconCode = cityWeatherResponse.weather[0].icon;
-        var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
-
-        var currentCity = $(`
-            <h2 id="currentCity">
-                ${cityWeatherResponse.name} ${today}  <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
-            </h2>
-            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
-            <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
-            <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
-        `);
-
-        $("#cityDetail").append(currentCity);
-
-        var lat = cityWeatherResponse.coord.lat;
-        var lon = cityWeatherResponse.coord.lon;
-        var uviQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-        $.ajax({
-            url: uviQueryURL,
-            method: "GET"
-        }).then(function(uviResponse) {
-            console.log(uviResponse);
-
-            var uvIndex = uviResponse.value;
-            var uvIndexP = $(`
-                <p>UV Index: 
-                    <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
-                </p>
-            `);
-
-            $("#cityDetail").append(uvIndexP);
-
-            futureCondition(lat, lon);
-
-            if (uvIndex >= 0 && uvIndex <= 2) {
-                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
-            } else if (uvIndex >= 3 && uvIndex <= 5) {
-                $("#uvIndexColor").css("background-color", "#FFF300");
-            } else if (uvIndex >= 6 && uvIndex <= 7) {
-                $("#uvIndexColor").css("background-color", "#F18B00");
-            } else if (uvIndex >= 8 && uvIndex <= 10) {
-                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
-            } else {
-                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
-            };  
-        });
-    });
-}
-    });
-};
-
-// function for future condition
 function futureCondition(lat, lon) {
 
-    // THEN I am presented with a 5-day forecast
     var futureURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
 
-    $.ajax({
-        url: futureURL,
-        method: "GET"
-    }).then(function(futureResponse) {
+    fetch(futureURL)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(futureResponse) {
         console.log(futureResponse);
         $("#fiveDay").empty();
         
@@ -110,11 +42,77 @@ function futureCondition(lat, lon) {
             `);
 
             $("#fiveDay").append(futureCard);
+
         }
     }); 
-}
+}})}
 
-// add on click event listener 
+var currentCondition = function(city) {
+
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    fetch(apiURL)
+    .then(function(response) {
+        if (response.ok) {
+        console.log(response);
+        response.json().then(function(cityWeatherResponse) {
+        console.log(cityWeatherResponse)
+        $("#weatherContent").css("display", "block");
+        $("#cityDetail").empty();
+        
+        var iconCode = cityWeatherResponse.weather[0].icon;
+        var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+        var currentCity = $(`
+            <h2 id="currentCity">
+                ${cityWeatherResponse.name} ${today}  <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
+            </h2>
+            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
+            <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
+            <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
+        `);
+
+        $("#cityDetail").append(currentCity);
+
+        var lat = cityWeatherResponse.coord.lat;
+        var lon = cityWeatherResponse.coord.lon;
+        
+        futureCondition(lat, lon);
+        
+        var uviURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+        return fetch(uviURL)
+        }).then(function(response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(uviResponse) {
+
+            var uvIndex = uviResponse.value;
+            var uvIndexP = $(`
+                <p>UV Index: 
+                    <span id="uvIndexColor" class="px-2 py-2">${uvIndex}</span>
+                </p>
+            `);
+
+            $("#cityDetail").append(uvIndexP);
+
+            if (uvIndex >= 0 && uvIndex <= 2) {
+                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
+            } else if (uvIndex >= 3 && uvIndex <= 5) {
+                $("#uvIndexColor").css("background-color", "#FFF300");
+            } else if (uvIndex >= 6 && uvIndex <= 7) {
+                $("#uvIndexColor").css("background-color", "#F18B00");
+            } else if (uvIndex >= 8 && uvIndex <= 10) {
+                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+            } else {
+                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
+            };  
+        });
+    }});
+}
+    });
+};
+
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
 
@@ -132,15 +130,11 @@ $("#searchBtn").on("click", function(event) {
     console.log(searchHistoryList);
 });
 
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
 $(document).on("click", ".list-group-item", function() {
     var listCity = $(this).text();
     currentCondition(listCity);
 });
 
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
 $(document).ready(function() {
     var searchHistoryArr = JSON.parse(localStorage.getItem("city"));
 
